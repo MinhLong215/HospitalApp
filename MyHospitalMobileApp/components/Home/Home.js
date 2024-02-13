f7ee12b5d0a544e19e0ac34d98e013bccc7a5fd3
MinhLong215 <2151010201long@ou.edu.vn>
2024-02-13 10:07:53 +0700

    het loi FE build dky dnhap

---
+++ MyHospitalMobileApp/components/Home/Home.js
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+// import Style from "./Style";
+// import React, { useEffect, useState } from "react";
+// import API, { endpoints } from "../../configs/API";
+import MyStyles from "../../styles/MyStyles";
+// import { TouchableOpacity } from "react-native-gesture-handler";
+// import moment from "moment";
+
+const Home = () => {
+    return (
+        <View style={MyStyles.container}>
+            <Text style={MyStyles.subject}>DANH MỤC KHOÁ HỌC</Text>
+        </View>
+    );
+}
+
+export default Home
---
+++ MyHospitalMobileApp/components/User/Login.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native"
+import MyStyles from "../../styles/MyStyles"
+
+
+const Login = () => {
+    return (
+        <View style={MyStyles.container}>
+            <Text style={MyStyles.title}>Login</Text>
+        </View>
+    )
+}
+
+export default Login
